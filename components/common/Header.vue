af035a6b4659056adadfc240355629728fdc3e3c
大西 純平 <waibandl321@oonishinoMac-mini.local>
2022-08-08 16:09:54 +0900

    header適用

--- components/common/Header.vue
+++ components/common/Header.vue
@@ -1,51 +1,49 @@
 <!-- eslint-disable eqeqeq -->
 <template>
   <v-app-bar
-      app
-      color="white"
-      flat
-    >
-      <v-container class="fill-height">
-        <v-btn
-          v-for="(link, idx) in header_links.common"
-          :key="idx"
-          :to="link.to"
-          text
-          nuxt
-        >
-          {{ link.text }}
-        </v-btn>
-        <div class="menu-wrap">
-          <div class="menu-first">
-            カテゴリー
-            <v-icon>mdi-chevron-down</v-icon>
-          </div>
-          <div class="menu-second">
-            <div
-              v-for="(category, index) in first_categories" 
-              :key="index"
-              class="menu-item p-relative"
-              @click.stop="clickCategoryMenu(category)"
-            >
-              <div>{{ category.name }}</div>
-            </div>
+    app
+    color="white"
+    flat
+  >
+    <v-container class="fill-height">
+      <v-btn
+        v-for="(link, idx) in header_links.common"
+        :key="idx"
+        :to="link.to"
+        text
+        nuxt
+      >
+        {{ link.text }}
+      </v-btn>
+      <div class="menu-wrap">
+        <div class="menu-first">
+          カテゴリー
+          <v-icon>mdi-chevron-down</v-icon>
+        </div>
+        <div class="menu-second">
+          <div
+            v-for="(category, index) in first_categories"
+            :key="index"
+            class="menu-item p-relative"
+            @click.stop="clickCategoryMenu(category)"
+          >
+            <div>{{ category.name }}</div>
           </div>
         </div>
-
-        <v-spacer></v-spacer>
-
-        <v-responsive max-width="260">
-          <v-text-field
-            dense
-            flat
-            hide-details
-            rounded
-            solo
-            append-icon="mdi-magnify"
-          ></v-text-field>
-        </v-responsive>
-      </v-container>
-    </v-app-bar>
+      </div>
+      <v-spacer></v-spacer>
+      <v-responsive max-width="260">
+        <v-text-field
+          dense
+          flat
+          hide-details
+          rounded
+          solo
+          append-icon="mdi-magnify"
+        ></v-text-field>
+      </v-responsive>
+    </v-container>
+  </v-app-bar>
 </template>
 <script>
 
@@ -54,55 +52,55 @@ export default {
   data: () => ({
     header_links: {
       categories: [],
-      category_base_url: "https://freelance321.com/wp-json/wp/v2/categories",
+      category_base_url: 'https://freelance321.com/wp-json/wp/v2/categories',
 
       common: [
         {
-          text: "TOP",
-          to: "/"
+          text: 'TOP',
+          to: '/'
         },
         {
-          text: "プライバシーポリシー",
-          to: "/privacy"
+          text: 'プライバシーポリシー',
+          to: '/privacy'
         },
         {
-          text: "お問い合わせ",
-          to: "/contact"
+          text: 'お問い合わせ',
+          to: '/contact'
         },
         {
-          text: "サイトマップ",
-          to: "/sitemap"
+          text: 'サイトマップ',
+          to: '/sitemap'
         },
         {
-          text: "プロフィール",
-          to: "/profile"
-        },
-      ],
+          text: 'プロフィール',
+          to: '/profile'
+        }
+      ]
     }
   }),
-  async fetch() {
+  async fetch () {
     const res = await this.$axios.get(
-      "https://freelance321.com/wp-json/wp/v2/categories?per_page=100"
+      'https://freelance321.com/wp-json/wp/v2/categories?per_page=100'
     )
     const items = []
-    const results = res.data;
-    results.forEach(item => {
+    const results = res.data
+    results.forEach((item) => {
       items.push(item)
-    });
+    })
     this.header_links.categories = items
   },
   computed: {
-    first_categories() {
+    first_categories () {
       return this.header_links.categories.filter(v => v.parent === 0)
     }
   },
   methods: {
-    clickCategoryMenu(category) {
+    clickCategoryMenu (category) {
       // storeにカテゴリー情報を格納する
-      this.$store.dispatch('setCategoryView', category);
+      this.$store.dispatch('setCategoryView', category)
       this.$router.push(category.slug)
     }
-  },
+  }
 }
 </script>
 <style scoped>
