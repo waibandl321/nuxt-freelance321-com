fa796643768ae6cb18b82b9d08408efdc64094b6
大西 純平 <waibandl321@oonishinoMac-mini.local>
2022-10-26 11:22:46 +0900

    axios呼び出しをmixinから除外(型推論エラー回避)

--- api/api.ts
+++ api/api.ts
@@ -1,4 +1,9 @@
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import { WP_API_PATH, CUSTOM_API_PATH } from '@/config/blog'
+
+Vue.use(VueAxios, axios)
 
 export const isWpApi = 'wp'
 
@@ -6,4 +11,13 @@ export const isCustomApi = 'custom'
 
 export function getApiBaseUrl (api_type: string) {
   return api_type === 'wp' ? WP_API_PATH : CUSTOM_API_PATH
+}
+
+export async function callGetApi (buildPath: string): Promise<any> {
+  console.log('call api:' + buildPath)
+  return await Vue.axios.get(buildPath, {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
 }
