5aa86613c037a8fb3de54069e141ed2ef4d0e570
大西 純平 <waibandl321@oonishinoMac-mini.local>
2022-10-26 09:36:56 +0900

    vue-property-decorator導入

---
+++ pages/test.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class Test extends Vue {
+  message: string = 'This is a message'
+}
+</script>
