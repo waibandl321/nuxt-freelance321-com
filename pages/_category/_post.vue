a710f8b7ea3deac98904e9c2f6ecc51ebc8ef9e7
大西 純平 <waibandl321@oonishinoMac-mini.local>
2022-11-12 10:58:18 +0900

    投稿ページTS修正

--- pages/_category/_post.vue
+++ pages/_category/_post.vue
@@ -1,6 +1,7 @@
 <!-- eslint-disable vue/no-v-html -->
 <template>
   <div>
+    <CommonMessageViewer :message="state.message" />
     <CommonLoadingPageInner v-if="state.loading" />
     <v-row v-else>
       <v-col cols="12" sm="9">
@@ -15,8 +16,9 @@
             {{ state.post.title.rendered }}
           </v-card-title>
           <div
+            v-if="htmlContent"
             class="post-content"
-            v-html="createHtml()"
+            v-html="htmlContent"
           />
         </div>
       </v-col>
@@ -28,70 +30,69 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, useFetch, reactive, useRoute } from '@nuxtjs/composition-api'
+import { defineComponent, computed, useFetch, reactive, useRoute } from '@nuxtjs/composition-api'
 import hljs from 'highlight.js'
 import { useFetchPost } from '@/utils/api'
 import type { Post, AxiosResponsePostObject } from '@/types'
 
 interface StateType {
   loading: boolean,
-  post: {} | Post,
+  post: Post,
   meta: {
     title: string
+  },
+  message: {
+    error: string
   }
 }
 
 export default defineComponent({
   layout: 'post',
   setup () {
+    const route = useRoute()
     const state = reactive<StateType>({
       loading: false,
       post: {},
       meta: {
         title: ''
+      },
+      message: {
+        error: ''
       }
     })
-    const route = useRoute()
 
     useFetch(async () => {
       state.loading = true
       try {
-        if (route.value.query.p) {
-          const response: AxiosResponsePostObject = await useFetchPost(route.value.query.p)
-          state.post = response.data
-        }
+        const response: AxiosResponsePostObject = await useFetchPost(route.value.query.p)
+        state.post = response.data
       } catch (error) {
-        console.log(error)
+        state.message.error = '投稿データ取得エラー: ' + error
       }
       state.loading = false
     })
 
-    const createHtml = () => {
-      const dom = document.createElement('div')
+    // エディタ整形
+    const htmlContent = computed<string | undefined>(() => {
+      const dom: HTMLDivElement = document.createElement('div')
       dom.innerHTML = state.post.content.rendered
-      try {
-        dom.querySelectorAll('.hcb_wrap pre').forEach((element) => {
-          const r = hljs.highlightAuto(element.textContent)
-          const lang = element.getAttribute('data-lang')
-          const code = element.querySelector('code')
-          code.innerHTML = r.value
-          code.classList.add('hljs', lang, 'language-' + lang)
-          // 邪魔な属性を削除する
-          const arrs = element.attributes
-          const arr_obj = []
-          for (const objAttrib of arrs) {
-            arr_obj.push(objAttrib.name)
-          }
-          arr_obj.forEach((a) => {
-            element.removeAttribute(a)
-          })
-        })
-      } catch {}
+      const hcb_elements: NodeListOf<Element> = dom.querySelectorAll('.hcb_wrap pre')
+
+      for (const element of Array.from(hcb_elements)) {
+        const code: (HTMLElement | null) = element.querySelector('code')
+        const lang: (string | null) = element.getAttribute('data-lang')
+        if (!lang || !code || !element.textContent) {
+          return
+        }
+        const result = hljs.highlightAuto(element.textContent)
+        code.innerHTML = result.value
+        code.classList.add('hljs', lang, 'language-' + lang)
+      }
       return dom.outerHTML
-    }
+    })
 
     return {
-      createHtml,
+      htmlContent,
       state,
       route
     }
